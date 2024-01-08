da047761704df19a6d91ddc5dbfbd21f2e1dcc0f
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2024-01-08 13:43:43 +0530

    Front face complete

---
+++ MiniProject/GmailHomepage/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Roboto': ['"Roboto"'],
+      'sans': ['"Open Sans"']
+    }
+  },
+  plugins: [],
+}
+
